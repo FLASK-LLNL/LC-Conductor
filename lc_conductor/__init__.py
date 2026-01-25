1dfb5d33396fecd1bbf0e9b2b235889a8c8fbb82
Brian C. Van Essen <vanessen1@llnl.gov>
2026-01-24 23:42:28 -0800

    Fixing how LC Conductor is installed as a package.

---
+++ lc_conductor/__init__.py
@@ -0,0 +1,24 @@
+from lc_conductor.backend_manager import ActionManager, TaskManager
+from lc_conductor.callback_logger import CallbackLogger
+from lc_conductor.tool_registration import (
+    ToolList,
+    list_server_urls,
+    list_server_tools,
+    validate_and_register_mcp_server,
+    check_registered_servers,
+    delete_registered_server,
+    get_registered_servers,
+)
+
+__all__ = [
+    "ActionManager",
+    "TaskManager", 
+    "CallbackLogger",
+    "ToolList",
+    "list_server_urls",
+    "list_server_tools",
+    "validate_and_register_mcp_server",
+    "check_registered_servers",
+    "delete_registered_server",
+    "get_registered_servers",
+]
