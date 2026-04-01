fd2627cd5b4e9c9fd52b10bfb5fa96f6d5d0dacb
Tom Benson <benson31@llnl.gov>
2026-04-01 16:57:32 -0400

    use pydantic dataclass

--- lc_conductor/backend_helper_function.py
+++ lc_conductor/backend_helper_function.py
@@ -1,11 +1,11 @@
 import asyncio
-from typing import Any, Dict, Optional, Literal, Tuple
-from dataclasses import dataclass, asdict
+from pydantic.dataclasses import dataclass
+from pydantic import Field
 
 
 @dataclass
 class RunSettings:
-    promptDebugging: bool = False
+    prompt_debugging: bool = Field(alias="promptDebugging", default=False)
 
 
 async def loop_executor(executor, func, *args, **kwargs):
